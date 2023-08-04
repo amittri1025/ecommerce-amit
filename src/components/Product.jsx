361c1ea93c6fd71bfadd349d355f8399a9e90960
01920802720 <tripathiamit2021@gmail.com>
2023-08-04 21:33:16 +0530

    Products Pages Complete Part-I , All images showing up

---
+++ src/components/Product.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { BsPlus } from "react-icons/bs";
+import { BsEyeFill } from "react-icons/bs";
+
+const Product = ({ product }) => {
+  const { id, image, title, category, price } = product;
+  return (
+    /* Here is just one block, with border and an image in it */
+    <div>
+      <div className="border border-[#e2e2e2] h-[300px] mb-4 relative overflow-hidden group transition">
+        <div
+          className="w-full h-full flex justify-center items-center"
+          key={id}
+        >
+          <img
+            className="max-h-[160px] group-hover:scale-110 transition-duration-300"
+            src={image}
+            alt=""
+          />
+        </div>
+        <div
+          className="absolute top-6 -right-11 group-hover:right-5 bg-red-500 p-2
+        flex flex-col items-center justify-center gap-y-2 opacity-0
+        group-hover:opacity-100 transition-all duration-300
+        "
+        >
+          <button>
+            <div className="flex justify-center items-center text-white w-12 h-12 bg-red-500 ">
+              <BsPlus className="text-3xl" />
+            </div>
+          </button>
+          <Link
+            to={`/product/${id}`}
+            className="w-12 h-12 bg-white flex justify-center items-center text-primary 
+            drop-shadow-xl"
+          >
+            <BsEyeFill />
+          </Link>
+        </div>
+      </div>
+      {/* category and price */}
+      <div>
+        <div className="text-sm capitalize text-gray-500 mb-1">
+          {category}
+        </div>
+        <Link to={`/products/${id}`}>
+            <h2 className="font-semibold mb-1">{title}</h2>
+        </Link>
+        <div className="font-semibold">$ {price}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
